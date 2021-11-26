1a8bd5c8ade6df246cf9ea7da333ba4ef0d113cf
Shreyas <rasaikarshreyas21@gmail.com>
2021-11-26 16:15:22 +0530

    broken our homepage into components,finished styling menu-items

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../componets/directory/directory";
+import "./homepage.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
