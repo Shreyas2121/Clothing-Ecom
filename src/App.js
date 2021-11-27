fb9dce0cc1460c3ed7cf31735dc5becea2045591
Shreyas <rasaikarshreyas21@gmail.com>
2021-11-27 12:36:07 +0530

    Added shop page,preview-collection.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.jsx";
+import { Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
