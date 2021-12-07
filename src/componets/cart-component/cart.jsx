4ddec15bf826afaa4b6d9b95922b23ce7acfbdf2
Shreyas <rasaikarshreyas21@gmail.com>
2021-12-07 17:17:11 +0530

    Reselect

--- src/componets/cart-component/cart.jsx
+++ src/componets/cart-component/cart.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 
 import CustomButton from "../custom-button/custom-button";
+import CartItem from "../cart-item/cart-item";
+import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart.scss";
 
-const CartDrop = () => (
+const CartDrop = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-item" />
+    <div className="cart-item">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDrop;
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDrop);
--- src/componets/collection-item/collection-item.jsx
+++ src/componets/collection-item/collection-item.jsx
@@ -20,7 +20,7 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton onclick={() => addItem(item)} inverted>
+      <CustomButton onClick={() => addItem(item)} inverted>
         ADD TO CART
       </CustomButton>
     </div>
