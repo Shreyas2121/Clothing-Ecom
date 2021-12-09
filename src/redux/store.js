aa36ba84e3ff74816096fbf6f2b0374a7269c9a2
Shreyas <rasaikarshreyas21@gmail.com>
2021-12-10 00:08:41 +0530

    Added redux persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootRedcuer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootRedcuer, applyMiddleware(...middlewares));
+export const store = createStore(rootRedcuer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
