3d59f249ebcd484d39b9b72ffd53a3f4b6880be7
Shreyas <rasaikarshreyas21@gmail.com>
2021-12-11 12:21:21 +0530

    Added styled components

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Directory from "../../componets/directory/directory";
-import "./homepage.scss";
+import { HomePageContainer } from "./homepage.styles";
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
